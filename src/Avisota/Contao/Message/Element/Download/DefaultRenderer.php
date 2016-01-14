9ae1495b9fd6aea19d29e943ff4aba46f8b450be
Sven Baumann <baumann.sv@gmail.com>
2016-01-14 14:43:21 +0100

    optimize imports

--- src/Avisota/Contao/Message/Element/Download/DefaultRenderer.php
+++ src/Avisota/Contao/Message/Element/Download/DefaultRenderer.php
@@ -16,10 +16,8 @@
 namespace Avisota\Contao\Message\Element\Download;
 
 use Avisota\Contao\Core\Message\Renderer;
-use Avisota\Contao\Entity\MessageContent;
 use Avisota\Contao\Message\Core\Event\AvisotaMessageEvents;
 use Avisota\Contao\Message\Core\Event\RenderMessageContentEvent;
-use Avisota\Recipient\RecipientInterface;
 use Contao\Doctrine\ORM\Entity;
 use Contao\Doctrine\ORM\EntityAccessor;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -34,7 +32,22 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class DefaultRenderer implements EventSubscriberInterface
 {
     /**
-     * {@inheritdoc}
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
      */
     static public function getSubscribedEvents()
     {
@@ -46,10 +59,12 @@ class DefaultRenderer implements EventSubscriberInterface
     /**
      * Render a single message content element.
      *
-     * @param MessageContent     $content
-     * @param RecipientInterface $recipient
+     * @param RenderMessageContentEvent $event
      *
      * @return string
+     * @internal param MessageContent $content
+     * @internal param RecipientInterface $recipient
+     *
      */
     public function renderContent(RenderMessageContentEvent $event)
     {
