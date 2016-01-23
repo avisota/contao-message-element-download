399ba12f45b198752ced7062fac08225bfba8691
Sven Baumann <baumann.sv@gmail.com>
2016-01-23 16:41:06 +0100

    make travis happy add clean phpunit test

---
+++ tests/Test/DefaultRendererTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-element-download
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+namespace Avisota\Test\Contao\Message\Element\Download;
+
+/**
+ * Class DefaultRendererTest
+ *
+ * @package Avisota\Test\Contao\Message\Element\Download
+ */
+class DefaultRendererTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRenderContent()
+    {
+    }
+}
